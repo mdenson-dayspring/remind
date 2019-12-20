4e300548020847b59ed5c686f36cb93258987d7b
Matthew Denson <mdenson@dayspring-tech.com>
2019-12-19 18:01:40 -0800

    Convert some "tests" to "specs"

--- src/__tests__/daynum.test.ts
+++ src/__tests__/daynum.test.ts
@@ -1,44 +1,46 @@
 import { dateToDaynum, daynum, daynumToYear } from '../index';
 import * as moment from 'moment';
 
-test('daynum calcs', () => {
-  expect(daynum(1970, 1, 1)).toEqual(1);
-  expect(daynum(2018, 12, 31)).toEqual(17897);
-  expect(daynum(2019, 1, 1)).toEqual(17897 + 1);
-  expect(daynum(2019, 2, 1)).toEqual(17897 + 31 + 1);
-  expect(daynum(2019, 3, 1)).toEqual(17897 + 31 + 28 + 1);
-  expect(daynum(2019, 12, 31)).toEqual(17897 + 365);
-  expect(daynum(2020, 1, 1)).toEqual(17897 + 365 + 1);
-  expect(daynum(2020, 3, 1)).toEqual(17897 + 365 + 31 + 29 + 1);
-  expect(daynum(2020, 12, 31)).toEqual(17897 + 365 + 366);
-  expect(daynum(2021, 1, 1)).toEqual(17897 + 365 + 366 + 1);
-  expect(daynum(2021, 3, 1)).toEqual(17897 + 365 + 366 + 31 + 28 + 1);
-  expect(daynum(2021, 12, 31)).toEqual(17897 + 365 + 366 + 365);
-});
+describe('daynum calcs', () => {
+  it('year,month,day to num', () => {
+    expect(daynum(1970, 1, 1)).toEqual(1);
+    expect(daynum(2018, 12, 31)).toEqual(17897);
+    expect(daynum(2019, 1, 1)).toEqual(17897 + 1);
+    expect(daynum(2019, 2, 1)).toEqual(17897 + 31 + 1);
+    expect(daynum(2019, 3, 1)).toEqual(17897 + 31 + 28 + 1);
+    expect(daynum(2019, 12, 31)).toEqual(17897 + 365);
+    expect(daynum(2020, 1, 1)).toEqual(17897 + 365 + 1);
+    expect(daynum(2020, 3, 1)).toEqual(17897 + 365 + 31 + 29 + 1);
+    expect(daynum(2020, 12, 31)).toEqual(17897 + 365 + 366);
+    expect(daynum(2021, 1, 1)).toEqual(17897 + 365 + 366 + 1);
+    expect(daynum(2021, 3, 1)).toEqual(17897 + 365 + 366 + 31 + 28 + 1);
+    expect(daynum(2021, 12, 31)).toEqual(17897 + 365 + 366 + 365);
+  });
 
-test('daynum calcs from date', () => {
-  expect(dateToDaynum(moment('2019-11-19').toDate())).toEqual(daynum(2019, 11, 19));
-  expect(dateToDaynum(moment('2020-03-12').toDate())).toEqual(daynum(2020, 3, 12));
-});
+  it('date to num', () => {
+    expect(dateToDaynum(moment('2019-11-19').toDate())).toEqual(daynum(2019, 11, 19));
+    expect(dateToDaynum(moment('2020-03-12').toDate())).toEqual(daynum(2020, 3, 12));
+  });
 
-test('daynum reverse calcs', () => {
-  expect(daynumToYear(1)).toEqual(1970);
-  expect(daynumToYear(365)).toEqual(1970);
-  expect(daynumToYear(365 + 1)).toEqual(1971);
-  expect(daynumToYear(365 + 365)).toEqual(1971);
-  expect(daynumToYear(365 + 365 + 1)).toEqual(1972);
-  expect(daynumToYear(365 + 365 + 366)).toEqual(1972);
-  expect(daynumToYear(365 + 365 + 366 + 1)).toEqual(1973);
-  expect(daynumToYear(365 + 365 + 366 + 365)).toEqual(1973);
-  expect(daynumToYear(17897)).toEqual(2018);
-  expect(daynumToYear(17897 + 1)).toEqual(2019);
-  expect(daynumToYear(17897 + 31 + 1)).toEqual(2019);
-  expect(daynumToYear(17897 + 31 + 28 + 1)).toEqual(2019);
-  expect(daynumToYear(17897 + 365)).toEqual(2019);
-  expect(daynumToYear(17897 + 365 + 1)).toEqual(2020);
-  expect(daynumToYear(17897 + 365 + 31 + 29 + 1)).toEqual(2020);
-  expect(daynumToYear(17897 + 365 + 366)).toEqual(2020);
-  expect(daynumToYear(17897 + 365 + 366 + 1)).toEqual(2021);
-  expect(daynumToYear(17897 + 365 + 366 + 31 + 28 + 1)).toEqual(2021);
-  expect(daynumToYear(17897 + 365 + 366 + 365)).toEqual(2021);
+  it('reverse daynum to year', () => {
+    expect(daynumToYear(1)).toEqual(1970);
+    expect(daynumToYear(365)).toEqual(1970);
+    expect(daynumToYear(365 + 1)).toEqual(1971);
+    expect(daynumToYear(365 + 365)).toEqual(1971);
+    expect(daynumToYear(365 + 365 + 1)).toEqual(1972);
+    expect(daynumToYear(365 + 365 + 366)).toEqual(1972);
+    expect(daynumToYear(365 + 365 + 366 + 1)).toEqual(1973);
+    expect(daynumToYear(365 + 365 + 366 + 365)).toEqual(1973);
+    expect(daynumToYear(17897)).toEqual(2018);
+    expect(daynumToYear(17897 + 1)).toEqual(2019);
+    expect(daynumToYear(17897 + 31 + 1)).toEqual(2019);
+    expect(daynumToYear(17897 + 31 + 28 + 1)).toEqual(2019);
+    expect(daynumToYear(17897 + 365)).toEqual(2019);
+    expect(daynumToYear(17897 + 365 + 1)).toEqual(2020);
+    expect(daynumToYear(17897 + 365 + 31 + 29 + 1)).toEqual(2020);
+    expect(daynumToYear(17897 + 365 + 366)).toEqual(2020);
+    expect(daynumToYear(17897 + 365 + 366 + 1)).toEqual(2021);
+    expect(daynumToYear(17897 + 365 + 366 + 31 + 28 + 1)).toEqual(2021);
+    expect(daynumToYear(17897 + 365 + 366 + 365)).toEqual(2021);
+  });
 });
--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,21 +1,23 @@
 import { parser, Reminder, toText } from '../index';
 
-test('parser', () => {
-  const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const e = [
-    { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
-    { month: 5, day: 19, message: "Matt's birthday" } as Reminder,
-    { year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder
-  ];
-  expect(parser(i)).toEqual(e);
-});
+describe('String manipulation should', () => {
+  it('parse text to Reminder', () => {
+    const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const e = [
+      { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
+      { month: 5, day: 19, message: "Matt's birthday" } as Reminder,
+      { year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder
+    ];
+    expect(parser(i)).toEqual(e);
+  });
 
-test('toText', () => {
-  const e = "12 6 Tiffany's birthday\n5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const i = [
-    { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
-    { month: 5, day: 19, message: "Matt's birthday" } as Reminder,
-    { year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder
-  ];
-  expect(toText(i)).toEqual(e);
+  it('format reminder as text', () => {
+    const e = "12 6 Tiffany's birthday\n5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const i = [
+      { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
+      { month: 5, day: 19, message: "Matt's birthday" } as Reminder,
+      { year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder
+    ];
+    expect(toText(i)).toEqual(e);
+  });
 });
