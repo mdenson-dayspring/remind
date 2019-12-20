d99547dffb683fb32a86519b494225e2f592e41a
Matthew Denson <mdenson@dayspring-tech.com>
2019-12-20 09:51:13 -0800

    more tests to spec

--- src/__tests__/mutate.test.ts
+++ src/__tests__/mutate.test.ts
@@ -1,35 +1,37 @@
 import { addReminder, daynum, purge, Reminder } from '../index';
 
-test('purge old none to purge', () => {
-  const i = "12 6 Tiffany's birthday\n 1 6 Epiphany\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 10, 31);
-  const e = [
-    { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
-    { month: 1, day: 6, message: 'Epiphany' } as Reminder,
-    { year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder
-  ];
-  expect(purge(i, today)).toEqual(e);
-});
-test('purge old remove one', () => {
-  const i = "12 6 Tiffany's birthday\n 1 6 Epiphany\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 12, 31);
-  const e = [
-    { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
-    { month: 1, day: 6, message: 'Epiphany' } as Reminder
-  ];
-  expect(purge(i, today)).toEqual(e);
-});
+describe('mutate reminder database', () => {
+  it('purge old none to purge', () => {
+    const i = "12 6 Tiffany's birthday\n 1 6 Epiphany\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 10, 31);
+    const e = [
+      { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
+      { month: 1, day: 6, message: 'Epiphany' } as Reminder,
+      { year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder
+    ];
+    expect(purge(i, today)).toEqual(e);
+  });
+  it('purge old remove one', () => {
+    const i = "12 6 Tiffany's birthday\n 1 6 Epiphany\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 12, 31);
+    const e = [
+      { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
+      { month: 1, day: 6, message: 'Epiphany' } as Reminder
+    ];
+    expect(purge(i, today)).toEqual(e);
+  });
 
-test('add reminder', () => {
-  const inReminder = { month: 1, day: 1, message: 'New Year' } as Reminder;
-  const inAccum = [
-    { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
-    { month: 1, day: 6, message: 'Epiphany' } as Reminder
-  ];
-  const expectAccum = [
-    { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
-    { month: 1, day: 6, message: 'Epiphany' } as Reminder,
-    { month: 1, day: 1, message: 'New Year' } as Reminder
-  ];
-  expect(addReminder(inAccum, inReminder)).toEqual(expectAccum);
+  it('add reminder', () => {
+    const inReminder = { month: 1, day: 1, message: 'New Year' } as Reminder;
+    const inAccum = [
+      { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
+      { month: 1, day: 6, message: 'Epiphany' } as Reminder
+    ];
+    const expectAccum = [
+      { month: 12, day: 6, message: "Tiffany's birthday" } as Reminder,
+      { month: 1, day: 6, message: 'Epiphany' } as Reminder,
+      { month: 1, day: 1, message: 'New Year' } as Reminder
+    ];
+    expect(addReminder(inAccum, inReminder)).toEqual(expectAccum);
+  });
 });
--- src/__tests__/query.test.ts
+++ src/__tests__/query.test.ts
@@ -1,40 +1,44 @@
 import { daynum, Reminder, todaysReminders } from '../index';
 
-test('filter next seven days 1', () => {
-  const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 11, 17);
-  const e = [{ year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder];
-  expect(todaysReminders(i, today)).toEqual(e);
-});
-test('filter next seven days 2', () => {
-  const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 11, 13);
-  const e = [{ year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder];
-  expect(todaysReminders(i, today)).toEqual(e);
-});
-test('filter next seven days 3', () => {
-  const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 11, 12);
-  const e = [] as Reminder[];
-  expect(todaysReminders(i, today)).toEqual(e);
-});
-
-test('filter next seven days repeating 1', () => {
-  const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 11, 30);
-  const e = [{ month: 12, day: 6, message: "Tiffany's birthday" } as Reminder];
-  expect(todaysReminders(i, today)).toEqual(e);
-});
-test('filter next seven days repeating 2', () => {
-  const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 11, 29);
-  const e = [] as Reminder[];
-  expect(todaysReminders(i, today)).toEqual(e);
+describe('filter next seven days (one time reminder)', () => {
+  it('item 2 days away (should return 1)', () => {
+    const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 11, 17);
+    const e = [{ year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder];
+    expect(todaysReminders(i, today)).toEqual(e);
+  });
+  it('item 7 days away (should return 1)', () => {
+    const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 11, 13);
+    const e = [{ year: 2019, month: 11, day: 19, message: 'Education Day 2019' } as Reminder];
+    expect(todaysReminders(i, today)).toEqual(e);
+  });
+  it('item 8 days away (should return none)', () => {
+    const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 11, 12);
+    const e = [] as Reminder[];
+    expect(todaysReminders(i, today)).toEqual(e);
+  });
 });
 
-test('filter next seven days repeating first week of Jan', () => {
-  const i = "12 6 Tiffany's birthday\n 1 6 Epiphany\n2019 11 19 Education Day 2019\n";
-  const today = daynum(2019, 12, 31);
-  const e = [{ month: 1, day: 6, message: 'Epiphany' } as Reminder];
-  expect(todaysReminders(i, today)).toEqual(e);
+describe('filter next seven days (repeating reminder)', () => {
+  it('item in 7 days (should return 1)', () => {
+    const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 11, 30);
+    const e = [{ month: 12, day: 6, message: "Tiffany's birthday" } as Reminder];
+    expect(todaysReminders(i, today)).toEqual(e);
+  });
+  it('1 item in 8 days (should return none)', () => {
+    const i = "12 6 Tiffany's birthday\n 5 19 Matt's birthday\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 11, 29);
+    const e = [] as Reminder[];
+    expect(todaysReminders(i, today)).toEqual(e);
+  });
+
+  it('1 item in 7 days, first week of Jan special case (should return 1)', () => {
+    const i = "12 6 Tiffany's birthday\n 1 6 Epiphany\n2019 11 19 Education Day 2019\n";
+    const today = daynum(2019, 12, 31);
+    const e = [{ month: 1, day: 6, message: 'Epiphany' } as Reminder];
+    expect(todaysReminders(i, today)).toEqual(e);
+  });
 });
