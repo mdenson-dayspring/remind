a831b4a040aff15a00e99d6ed788787e355ebd1a
Matthew Denson <mdenson@dayspring-tech.com>
2019-12-20 16:32:05 -0800

    update dependencies

--- src/cli/__tests__/remind-cli.test.ts
+++ src/cli/__tests__/remind-cli.test.ts
@@ -2,12 +2,19 @@ import { main } from '../remind-cli';
 import * as moment from 'moment';
 import { existsSync, unlinkSync, copyFileSync, readFileSync } from 'fs';
 
+let logOutput = '';
+let errorOutput = '';
+let mockLog = (inputs: any) => (logOutput += inputs);
+const mockError = (inputs: any) => (errorOutput += inputs);
+
+beforeAll(() => {
+  console['log'] = jest.fn(mockLog);
+  console['error'] = jest.fn(mockError);
+});
+
 describe('The CLI should display reminder if they exist', () => {
-  let logOutput = '';
-  let mockLog = (inputs: any) => (logOutput += inputs);
   beforeEach(() => {
     logOutput = '';
-    console['log'] = jest.fn(mockLog);
   });
   it('should print reminders', async () => {
     expect(await main('', './fixtures/one', moment('2019-11-19').toDate())).toBe(0);
@@ -20,8 +27,13 @@ describe('The CLI should display reminder if they exist', () => {
   it('should create blank file if it did not exist', async () => {
     expect(await main('', './fixtures/three', moment('2019-11-19').toDate())).toBe(0);
 
-    if (existsSync('./fixtures/three')) {
+    // expect the file to be created and blank
+    const fileExists = existsSync('./fixtures/three');
+    expect(fileExists).toBeTruthy();
+    if (fileExists) {
+      const newFile = readFileSync('./fixtures/three', 'utf8');
       unlinkSync('./fixtures/three');
+      expect(newFile).toBe('');
     }
 
     expect(logOutput).toBe('\n');
@@ -29,15 +41,9 @@ describe('The CLI should display reminder if they exist', () => {
 });
 
 describe('The CLI should report errors in parameters', () => {
-  let logOutput = '';
-  const mockLog = (inputs: any) => (logOutput += inputs);
-  let errorOutput = '';
-  const mockError = (inputs: any) => (errorOutput += inputs);
   beforeEach(() => {
     logOutput = '';
-    console['log'] = jest.fn(mockLog);
     errorOutput = '';
-    console['error'] = jest.fn(mockError);
   });
   it('print usage when gets bad param', async () => {
     expect(await main('23', './fixtures/one', moment('2019-11-19').toDate())).toBe(1);
@@ -47,42 +53,32 @@ describe('The CLI should report errors in parameters', () => {
 });
 
 describe('The CLI should', () => {
-  let logOutput = '';
-  const mockLog = (inputs: any) => (logOutput += inputs);
-  let errorOutput = '';
-  const mockError = (inputs: any) => (errorOutput += inputs);
   beforeEach(() => {
     logOutput = '';
-    console['log'] = jest.fn(mockLog);
     errorOutput = '';
-    console['error'] = jest.fn(mockError);
   });
   it('add reminder', async () => {
     copyFileSync('./fixtures/one', './fixtures/temp');
     expect(await main('11 11 Vetrans Day', './fixtures/temp', moment('2019-11-19').toDate())).toBe(0);
-
-    const expectFile = '2019 11 20 MW - sprint 1.0.43 to prod\n' + '11 23 Sunday\n' + '11 11 Vetrans Day\n';
     const newFile = readFileSync('./fixtures/temp', 'utf8');
-    expect(newFile).toEqual(expectFile);
-
     if (existsSync('./fixtures/temp')) {
       unlinkSync('./fixtures/temp');
     }
+
+    const expectFile = '2019 11 20 MW - sprint 1.0.43 to prod\n' + '11 23 Sunday\n' + '11 11 Vetrans Day\n';
+    expect(newFile).toEqual(expectFile);
     expect(logOutput).toBe('add reminder "11 11 Vetrans Day"');
     expect(errorOutput).toBe('');
   });
   it('add reminder and purge old reminders', async () => {
     copyFileSync('./fixtures/one', './fixtures/temp');
     expect(await main('11 11 Vetrans Day', './fixtures/temp', moment('2019-11-21').toDate())).toBe(0);
-
-    const expectFile = '11 23 Sunday\n' + '11 11 Vetrans Day\n';
     const newFile = readFileSync('./fixtures/temp', 'utf8');
-    expect(newFile).toEqual(expectFile);
-
     if (existsSync('./fixtures/temp')) {
       unlinkSync('./fixtures/temp');
     }
-    expect(logOutput).toBe('add reminder "11 11 Vetrans Day"');
-    expect(errorOutput).toBe('');
+
+    const expectFile = '11 23 Sunday\n' + '11 11 Vetrans Day\n';
+    expect(newFile).toEqual(expectFile);
   });
 });
